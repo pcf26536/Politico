98116fcb09efc5b01b7edb3e388275942613d701
pcf26536 <kadanieet>
2019-02-01 18:48:15 +0300

    Feature Admin can delete parties [finish #163587355]

---
+++ scripts/admin_parties.js
@@ -0,0 +1,21 @@
+function showAddForm() {
+    document.getElementById('add_party_form').style.display = "block";
+    document.getElementById('add_party').style.display = "none";
+    document.getElementById('party_list').style.display = "none";
+}
+
+document.getElementById('cancel_add_party').onclick = function () {
+    document.getElementById('add_party_form').style.display = "none";
+    document.getElementById('add_party').style.display = "block";
+    document.getElementById('party_list').style.display = "block";
+}
+
+function showDelete() {
+    /*elems = document.getElementsByClassName('hide');
+    alert(elems.length);
+    for(var i = 0; i < elems.length; i++) {
+        elems[i].style.display = "inline";
+    }*/
+    document.getElementById('delete').style.display = "block";
+    document.getElementById('add_party').style.display = "none";
+}
