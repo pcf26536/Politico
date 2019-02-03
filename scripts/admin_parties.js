8ed57f0c1bc534cd4d6f5c5915780dafaaecded9
pcf26536 <kadanieet>
2019-02-03 15:43:18 +0300

    Altered the admin's add party page

--- scripts/admin_parties.js
+++ scripts/admin_parties.js
@@ -1,7 +1,9 @@
 function showAddForm() {
-    document.getElementById('add_party_form').style.display = "block";
-    document.getElementById('add_party').style.display = "none";
-    document.getElementById('party_list').style.display = "none";
+    //document.getElementById('add_party_form').style.display = "block";
+    //document.getElementById('add_party').style.display = "none";
+    //document.getElementById('party_list').style.display = "none";
+    showById('add_party_form', block);
+    hideById(['add_party', 'party_list']);
 }
 
 document.getElementById('cancel_add_party').onclick = function () {
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,14 +1,25 @@
 var inline = "inline";
 var block = "block";
+var object = 'object';
+var string = 'string';
 
-/*hide elements functions*/
+// returns the type of variable
+function typeOf(variable) {
+    return typeof variable;
+}
+
+                     /*hide elements functions*/
 function hide(element) {
     element.style.display = "none";
 }
 
 // This function hides an element by Id
 function hideById(element_id) {
-    hide(document.getElementById(element_id));
+    var type = typeOf(element_id);
+    if (type == object) {
+        for(var i = 0; i < element_id.length; i++) { hide(document.getElementById(element_id[i])); }
+    } 
+    else if(type == string) { hide(document.getElementById(element_id)); }
 }
 
 // This function hides an element by class
@@ -18,23 +29,24 @@ function hideByClass(class_name) {
         hide(elements[i]);
     }
 }
-/*hide elements functions*/
+                    /*hide elements functions*/
 
-/*Show elements functions*/
+
+                    /*Show elements functions*/
 function show(element, type) {
     element.style.display = type;
 }
 
 // This function hides an element by Id
 function showById(element_id, type) {
-    hide(document.getElementById(element_id, type));
+    show(document.getElementById(element_id), type);
 }
 
 // This function hides an element by class
 function showByClass(class_name, type) {
     elements = document.getElementsByClassName(class_name);
     for(var i = 0; i < elements.length; i++) {
-        hide(elements[i], type);
+        show(elements[i], type);
     }
 }
-/*Show elements functions*/
+                /*Show elements functions*/
