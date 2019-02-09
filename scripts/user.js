b74a14b36c659d05cb803a88436c69e2afb412f8
pcf26536 <kadanieet>
2019-02-09 18:43:20 +0300

    added alerts, refactored redirects

--- scripts/user.js
+++ scripts/user.js
@@ -5,43 +5,18 @@ if (getUrlParam('signin')) {
     showAlert('success', makeAlertMessage('Success!', "You're now logged in."));
 }
 
-else if (getUrlParam('reset')) {
-  showAlert('success', makeAlertMessage('Success!', "Password reset successful, directing to <a href='signin.html'>login</a> page"));
+else if (getUrlParam('reset')) { // needs fixig
+  //showAlert('success', makeAlertMessage('Success!', "Password reset successful, directing to <a href='signin.html'>login</a> page"));
+  showAlert('success', makeAlertMessage('Success!', "Password reset successful"));
   //redirect("signin.html");
-  setTimeout(
-    redirect("signin.html"), 5000);
+  //setTimeout(redirect("signin.html"), 5000);
 }
 
-elseif (getUrlParam('add_party')) {
+else if (getUrlParam('add_party')) {
   showAlert('success', makeAlertMessage('Success!', "Political party has been added"));
 }
 
-if (getUrlParam('add_office')) {
+else if (getUrlParam('add_office')) {
   showAlert('success', makeAlertMessage('Success!', "Political party has been added"));
 }
-
-/*//var referer = getUrlParam('signin','Empty');
-//alert(referer);
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the <span> element that closes the modal
-var span1 = document.getElementsByClassName("close")[0];
-
-//after sign in show the modal
-if (getUrlParam('signin') || getUrlParam('reset') || getUrlParam('add_party') || getUrlParam('add_office')) {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}; */
 
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -3,11 +3,40 @@ var block = "block";
 var object = 'object';
 var string = 'string';
 
+// shows the menu icon on small width
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+            /* get the get url params */
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
+
+function getUrlParam(parameter){
+  var urlparameter = '';
+  if(window.location.href.indexOf(parameter) > -1){
+      urlparameter = getUrlVars()[parameter];
+      }
+      console.log(urlparameter);
+  return urlparameter;
+}
+            /* get the get url params */
+            
+
 // returns the type of variable
 function typeOf(variable) {
     return typeof variable;
 }
-
 
 // redirection
 function redirect(url) {
