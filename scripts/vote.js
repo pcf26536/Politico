cba64662da8144ee7a646a8f3c49c8c2f7f262d0
wainainad60 <muffwaindan@gmail.com>
2019-02-10 18:28:42 +0300

    updated user vote page

--- scripts/vote.js
+++ scripts/vote.js
@@ -1,4 +1,37 @@
-// Get the modal
+function listHide() {
+  if(getById('officeList').className=='office-list responsive') myFunction('results');
+}
+
+var offices = getByClass('office');
+
+for (var x = 0; x < offices.length; x++) {
+  offices[x].onclick = function () {
+      //hideById('all-results');
+      showById('all-candidates', block);
+      hideByClass('candidates-item');
+      showById(this.getAttribute('id') + '-candidates', block);
+      listHide();
+  };
+}
+
+function showAllCandidates() {
+  showById('all-candidates', block);
+  showByClass('candidates-item', block); 
+  listHide();
+}
+
+getById('show-all').onclick = showAllCandidates;
+
+getByClass("closebtn")[0].onclick = function(){
+  // Get the parent of <span class="closebtn"> (<div class="alert">)
+  var div = this.parentElement;
+
+  // Set the opacity of div to 0 (transparent)
+  div.style.opacity = "0";
+};
+
+
+/*/ Get the modal
 var modal = document.getElementById('vote_message');
 
 // Get the button that opens the modal
@@ -23,3 +56,4 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
+*/
